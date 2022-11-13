4c22a425023771eaf8779fd854970efc3acaa5e9
“Mazlum <mazlum09kaya@gmail.com>
2022-11-13 20:56:28 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace tekrar5_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(246, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Metotların aşırı yüklenmesi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(264, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(360, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LimeGreen;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(244, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Bazı matematiksel metotlar";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(108, 255);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 410);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekrar5_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //[1] iki tane parametreleri farklı isimleri aynı metot - aşırı yüklenme
+        public void metot1(int s1, int s2)
+        {
+            int carpim = s1 * s2;
+            label2.Text = carpim.ToString();
+        }
+
+        public void metot1(int s1, int s2, int s3)
+        {
+            int carpim = s1 * s2 * s3;
+            label2.Text = carpim.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            metot1(10, 2);
+            //metot1(20, 2, 3);
+        }
+
+        //[2] iki tane parametrelerindeki değişken türü farklı isimleri aynı metot - aşırı yüklenme
+        public void metot2(int a, int b)
+        {
+            int s = a * b;
+            label3.Text = s.ToString(); 
+        }
+        public void metot2(double a, double b)
+        {
+            double s = a * b;
+            label3.Text = s.ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //metot2(2, 4);
+            metot2(2.0, 2.5);
+        }
+
+        // [2] Bazı matematiksel metotlar
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // üs alır
+            /*double sonuc = Math.Pow(2, 3);
+            label1.Text = sonuc.ToString();*/
+
+
+            //karekör alır
+            /*double sonuc = Math.Sqrt(2);
+            label1.Text = sonuc.ToString();*/
+
+            //mutlak değer alır
+            /*double sonuc = Math.Abs(100);
+            label1.Text = sonuc.ToString();*/
+
+            //yuvarlar / aşağı doğru yuvarlar
+            /*double sonuc = Math.Floor(19.98);
+            label1.Text = sonuc.ToString(); */
+
+            //yukarı yuvarlar
+            /*double sonuc = Math.Ceiling(1.04);
+            label1.Text = sonuc.ToString();*/
+
+            //yakına yuvarlar
+            /*double sonuc = Math.Round(1.54);
+            label1.Text = sonuc.ToString();*/
+
+            //min sayıyı bulur
+            /*double sonuc = Math.Min(10 , 54);
+            label1.Text = sonuc.ToString(); */
+
+            // max sayıyı bulur
+            double sonuc = Math.Max(5, 8);
+            label1.Text = sonuc.ToString();
+        }
+    }
+}
